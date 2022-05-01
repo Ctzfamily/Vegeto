084280670c897ff2e912105423a35182dc6a53e3
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-05-01 14:26:37 +0530

    Update inlinefuncs.py

--- VegetaRobot/utils/inlinefuncs.py
+++ VegetaRobot/utils/inlinefuncs.py
@@ -44,7 +44,7 @@ from pyrogram.types import (
 )
 from search_engine_parser import GoogleSearch
 
-from wbb import (
+from VegetaRobot import (
     BOT_USERNAME,
     MESSAGE_DUMP_CHAT,
     SUDOERS,
@@ -55,13 +55,13 @@ from wbb import (
     app2,
     arq,
 )
-from wbb.core.keyboard import ikb
-from wbb.core.tasks import _get_tasks_text, all_tasks, rm_task
-from wbb.core.types import InlineQueryResultCachedDocument
-from wbb.modules.info import get_chat_info, get_user_info
-from wbb.modules.music import download_youtube_audio
-from wbb.utils.functions import test_speedtest
-from wbb.utils.pastebin import paste
+from VegetaRobot.core.keyboard import ikb
+from VegetaRobot.core.tasks import _get_tasks_text, all_tasks, rm_task
+from VegetaRobot.core.types import InlineQueryResultCachedDocument
+from VegetaRobot.modules.info import get_chat_info, get_user_info
+from VegetaRobot.modules.music import download_youtube_audio
+from VegetaRobot.utils.functions import test_speedtest
+from VegetaRobot.utils.pastebin import paste
 
 keywords_list = [
     "image",
